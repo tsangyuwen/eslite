b7ccd576edca4edeca120e690d1cf8c0b5d011d7
tsangyuwen <tsangyuwen@gmail.com>
2020-03-09 16:22:29 +0800

    feat(devise): add devise gem and create staff

--- Gemfile
+++ Gemfile
@@ -28,6 +28,9 @@ gem 'jbuilder', '~> 2.7'
 # Use aasm for state machine
 gem 'aasm'
 
+# Use devise for authenication
+gem 'devise'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :staffs, path: "staffs", controllers: {
+    sessions: 'admin/staffs/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
